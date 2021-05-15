1eeac0cc8522a59809c3e3d46004dc7dbd8dede8
Skchkov <skchkov@yandex.ru>
2021-05-15 11:00:00 +0300

    24 - useEffect clenup

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -30,15 +30,78 @@ export const SimpleExample = () => {
     );
 };
 
-export const SetTimeOutExample = () => {
+export const SetIntervalExample = () => {
     const [counter, setCounter] = useState(1);
     console.log("setTimeOut example");
 
     useEffect(() => {
-        setInterval(() => {
+        const interval = setInterval(() => {
             setCounter((state) => state + 1);
         }, 1000);
+        return () => {
+            clearInterval(interval);
+        };
     }, []);
 
     return <div>counter: {counter}</div>;
+};
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1);
+    console.log("component rendered");
+
+    useEffect(() => {
+        console.log("Effect done " + counter);
+
+        return () => {
+            console.log("reset effect");
+        };
+    }, [counter]);
+
+    const increase = () => {
+        setCounter(counter + 1);
+    };
+
+    return (
+        <div>
+            counter: {counter} <button onClick={increase}>+</button>
+        </div>
+    );
+};
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState("");
+    console.log("component rendered with" + text);
+
+    const handler = (e: KeyboardEvent) => {
+        console.log(e.key);
+        setText(text + e.key);
+    };
+
+    useEffect(() => {
+        window.addEventListener("keypress", handler);
+        return () => {
+            window.removeEventListener("keypress", handler);
+        };
+    }, [text]);
+
+    return <div>Typed text: {text}</div>;
+};
+
+export const SetTimeOutExample = () => {
+    const [text, setText] = useState("");
+    console.log("component rendered with" + text);
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            console.log("timeout expared");
+            setText("3 seconds passed");
+        }, 3000);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [text]);
+
+    return <div>Typed text: {text}</div>;
 };
